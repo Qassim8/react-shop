8e2287cb993038a20c3e1358ce37a8b8bc019d70
mohammed elgasim <mohammedalgasim123@gmail.com>
2025-01-12 10:45:35 +0200

    change some style

--- src/components/EmptyCart.jsx
+++ src/components/EmptyCart.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function EmptyCart() {
   return (
     <div className="d-flex flex-column gap-2 justify-content-center align-items-center mt-0 pt-0">
-      <div style={{width: "350px"}}>
+      <div style={{width: "250px"}}>
         <ShoppingBagOutlined className="w-100 h-100"/>
       </div>
       <h3 className="fw-light">Your cart empty..! go to shop and add some products</h3>
